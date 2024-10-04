d9592211592b8416882a4739d5f5043d33f412d1
JakeT762 <1117358@apps.nsd.org>
2024-10-04 21:19:08 +0000

    done

--- src/main/java/org/nhs/app/App.java
+++ src/main/java/org/nhs/app/App.java
@@ -1,8 +1,46 @@
-/*----------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See LICENSE in the project root for license information.
- *---------------------------------------------------------------------------------------*/
+public class App {
+    public static void main(String[] args) {
+        /*for (int x = 1; x <= 5; x++) {
+            for (int y = 1; y <= 4; y++) {
+                System.out.print(x);
+            }
 
-package org.nhs.app;
+            System.out.println();
+        }*/
 
-// Your code goes here
+        /*for (int num = 1; num <= 5; num++) {
+            for (int count = 1; count <= num; count++) {
+                System.out.print(num);
+            }
+
+            System.out.println();
+        } */
+
+        /*for (int num = 1; num <= 5; num++) {
+            for (int dot = 5 - num; dot > 0; dot--) {
+                System.out.print(".");
+
+            }
+
+            for (int count = num; count > 0; count--) {
+                System.out.print(num);
+            }
+
+            System.out.println();
+        } */
+
+        for (int num = 1; num <= 5; num++) {
+            for (int dot1 = 5 - num; dot1 > 0; dot1--) {
+                System.out.print(".");
+            }
+
+            System.out.print(num);
+
+            for (int dot2 = num - 1; dot2 > 0; dot2--) {
+                System.out.print(".");
+            }
+
+            System.out.println();
+        }
+    }
+}
